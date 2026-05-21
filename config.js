f6cae654d5995ba98570cd1b9176647a900ae5a5
clarencechien <7018587+clarencechien@users.noreply.github.com>
2026-05-21 15:51:40 +0800

    Add leader payment info and LINE User ID to config

--- config.js
+++ config.js
@@ -5,5 +5,14 @@ const CONFIG = {
     LIFF_ID: "2010150112-ejdkGN7h",
     
     // 2. 您的 Google Apps Script 網頁應用程式正式 URL (必須以 /exec 結尾)
-    GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbygmumhGW6KaWj1Jbc7ZSo8IeKxrdyPcJWpJKeALvFZEqo_McMxYpzJbP6kbO81Q3eV/exec"
+    GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbygmumhGW6KaWj1Jbc7ZSo8IeKxrdyPcJWpJKeALvFZEqo_McMxYpzJbP6kbO81Q3eV/exec",
+
+    // 3. 團長預設收款資訊 (抽離至設定檔，不需在 HTML 中硬編碼)
+    LEADER_BANK_CODE: "012",             // 台北富邦銀行
+    LEADER_ACCOUNT: "0081680005617649",    // 團長預設收款帳號 (14碼)
+    
+    // 4. 合法管理員/團長的 LINE User ID 
+    // 當 LIFF 登入者的 ID 與此相符，才會顯示「團長主控台」頁籤。
+    // (您可以先從試算表的 SystemLogs 裡，找到您發送「開團」訊息時的 From: Uxxxxxxxx 亂碼並填入此處)
+    LEADER_LINE_ID: "U7f4094a8f5497bfda6f47044a9e2686d" 
 };
