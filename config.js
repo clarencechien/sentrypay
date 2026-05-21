c8bc6514adefc199915ef2d74c853c1f86e43c52
clarencechien <7018587+clarencechien@users.noreply.github.com>
2026-05-21 15:43:01 +0800

    Update LIFF_ID and GAS_WEB_APP_URL in config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// ==================== 辦公室點餐助手設定檔 ====================
+// 您可以自由修改此檔案中的參數，不需要重新部署網頁 (liff_app.html)！
+const CONFIG = {
+    // 1. 您的 LINE LIFF ID (例如: "2010151157-g6292b67")
+    LIFF_ID: "2010150112-ejdkGN7h",
+    
+    // 2. 您的 Google Apps Script 網頁應用程式正式 URL (必須以 /exec 結尾)
+    GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbygmumhGW6KaWj1Jbc7ZSo8IeKxrdyPcJWpJKeALvFZEqo_McMxYpzJbP6kbO81Q3eV/exec"
+};
